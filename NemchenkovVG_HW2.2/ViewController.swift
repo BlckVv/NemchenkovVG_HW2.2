a160281e986da679c69cfedb5ded672b74fda908
NemchenkovVG <blckvv@yandex.ru>
2021-07-12 02:29:06 +0400

    New commit

--- NemchenkovVG_HW2.2/ViewController.swift
+++ NemchenkovVG_HW2.2/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var red: UILabel!
+    @IBOutlet weak var green: UILabel!
+    @IBOutlet weak var blue: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var rgbView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        red.text = String(redSlider.value)
+        green.text = String(greenSlider.value)
+        blue.text = String(blueSlider.value)
+        
+    }
+    
+    
+    @IBAction func rgbAction() {
+        
+        red.text = String(round(100 * redSlider.value) / 100)
+        green.text = String(round(100 * greenSlider.value) / 100)
+        blue.text = String(round(100 * blueSlider.value) / 100)
+
+        let redSliderValue = CGFloat(redSlider.value)
+        let greenSliderValue = CGFloat(greenSlider.value)
+        let blueSliderValue = CGFloat(blueSlider.value)
+        
+        rgbView.backgroundColor = UIColor(red: redSliderValue, green: greenSliderValue, blue: blueSliderValue, alpha: 1)
     }
 
-
 }
-
